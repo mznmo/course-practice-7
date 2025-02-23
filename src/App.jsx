f329ef1731a36e3a88442e4f905d73cb0195d740
mznmo <102366479+mznmo@users.noreply.github.com>
2025-02-23 16:09:26 +0200

    place-picker done

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,40 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect, useCallback } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
+import { sortPlacesByDistance } from './loc.js';
+
+const storedIDS = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+const storedPlaces = storedIDS.map((id) => AVAILABLE_PLACES.find((place) => place.id === id)); 
 
 function App() {
-  const modal = useRef();
   const selectedPlace = useRef();
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
+
+  
+  useEffect(() => { //executes after component execution is completed
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(AVAILABLE_PLACES, position.coords.latitude, position.coords.longitude);
+      setAvailablePlaces(sortedPlaces);
+    }); //getting location of user and sorting places by closest
+  }, []); //executes the function again if the value in the array has changed
+
+  
 
   function handleStartRemovePlace(id) {
-    modal.current.open();
+    setModalIsOpen(true);
     selectedPlace.current = id;
   }
 
   function handleStopRemovePlace() {
-    modal.current.close();
+    setModalIsOpen(false);
   }
+
 
   function handleSelectPlace(id) {
     setPickedPlaces((prevPickedPlaces) => {
@@ -28,19 +44,28 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIDS = JSON.parse(localStorage.getItem('selectedPlaces', id)) || [];
+    if(storedIDS.indexOf(id) === -1) {
+    localStorage.setItem('selectedPlaces', JSON.stringify([id, ...storedIDS])); //another example of side effect
+    }
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
-    modal.current.close();
-  }
+    setModalIsOpen(false);
+
+    const storedIDS = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    localStorage.setItem('selectedPlaces', JSON.stringify(storedIDS.filter((id) => id !== selectedPlace.current)));
+  }, []);
+  
 
   return (
     <>
-      <Modal ref={modal}>
-        <DeleteConfirmation
+      <Modal open={modalIsOpen}>
+            <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
         />
@@ -63,7 +88,8 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={availablePlaces}
+          fallbackText="Sorting places by distance..."
           onSelectPlace={handleSelectPlace}
         />
       </main>
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,23 +1,22 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { forwardRef, useRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
-const Modal = forwardRef(function Modal({ children }, ref) {
+const Modal = forwardRef(function Modal({ open, children }) {
   const dialog = useRef();
 
-  useImperativeHandle(ref, () => {
-    return {
-      open: () => {
-        dialog.current.showModal();
-      },
-      close: () => {
-        dialog.current.close();
-      },
-    };
-  });
+
+  useEffect(() => {
+  if(open === true) {
+    dialog.current.showModal();
+  } else {
+    dialog.current.close();
+  }
+}, [open]) //re executes because the open changes (with each change of open the useEffect executes again)
+
 
   return createPortal(
     <dialog className="modal" ref={dialog}>
-      {children}
+      {open ? children : null}
     </dialog>,
     document.getElementById('modal')
   );
